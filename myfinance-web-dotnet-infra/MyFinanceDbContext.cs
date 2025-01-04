8a5bf35b3bdda642e870fa2bf673776115a2e3e4
Cluyverth W. <ContatoCluyverth@gmail.com>
2025-01-04 14:37:53 -0300

    refactor: change connection string

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -11,7 +11,7 @@ namespace myfinance_web_dotnet_infra
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=myfinance;User Id=sa;Password=Sa!teste@123456;");
+            optionsBuilder.UseSqlServer(@"Server=172.17.0.3,1433;Database=myfinance;User Id=sa;Password=Sa!teste@123456;TrustServerCertificate=True;");
         }
     }
 }
