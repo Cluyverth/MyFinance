a96acb03af4bbb0245af998522903c70390d7023
Cluyverth W. <ContatoCluyverth@gmail.com>
2025-02-08 00:41:52 -0300

    refactor: add interface to infra

---
+++ myfinance-web-dotnet-infra/Interfaces/IPlanoContaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myfinance_web_dotnet_domain.Entities;
+using myfinance_web_dotnet_infra.Interfaces.Base;
+
+namespace myfinance_web_dotnet_infra.Interfaces
+{
+    public interface IPlanoContaRepository : IRepository<PlanoConta>
+    {
+
+    }
+}
